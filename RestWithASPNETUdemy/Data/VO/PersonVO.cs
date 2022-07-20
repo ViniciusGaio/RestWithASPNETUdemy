434f453527101063d33655b25413408eefa0cfd6
vinicius.moncorvo <vinagamer9@gmail.com>
2022-07-20 13:14:08 -0300

    Update(102-104): Custom Serialization

--- RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,13 +1,19 @@
 ﻿using RestWithASPNETUdemy.Model.Base;
+using System.Text.Json.Serialization;
 
 namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("Code")]
         public int Id { get; set; }
+        [JsonPropertyName("Name")]
         public string FirstName { get; set; }
+        [JsonPropertyName("Last Name")]
         public string LastName { get; set; }
+        [JsonIgnore]
         public string Address { get; set; }
+        [JsonPropertyName("Sex")]
         public string Gender { get; set; }
 
     }
