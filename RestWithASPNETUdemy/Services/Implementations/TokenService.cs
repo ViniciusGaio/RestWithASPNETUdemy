384e8c7f56a84ca6b910e0cb16dbb87762ccb11f
vinicius.moncorvo <vinagamer9@gmail.com>
2022-07-25 14:36:38 -0300

    Update(129): start Implementing TokenService

---
+++ RestWithASPNETUdemy/Services/Implementations/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RestWithASPNETUdemy.Configurations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class TokenService : ITokenService
+    {
+        private readonly TokenConfiguration _configuration;
+
+        public TokenService(TokenConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var Options = new JwtSecurityToken(
+                issuer: _configuration.Issuer,
+                audience: _configuration.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_configuration.Minutes),
+                signingCredentials: signinCredentials
+                );
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(Options);
+
+            return tokenString; 
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
